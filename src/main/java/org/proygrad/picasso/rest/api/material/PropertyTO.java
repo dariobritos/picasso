e379138192cf053435f3db9b06350838a9ee349a
Kolo <vidiaz@despegar.com>
2018-04-04 10:32:01 -0300

    agrega mock y TO para materiales

---
+++ src/main/java/org/proygrad/picasso/rest/api/material/PropertyTO.java
@@ -0,0 +1,69 @@
+package org.proygrad.picasso.rest.api.material;
+
+public class PropertyTO {
+
+    private String code;
+    private Double value;
+    private String unit;
+    private String type;
+    private PropertyDistributionTO distribution;
+    private String magnitude;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public PropertyDistributionTO getDistribution() {
+        return distribution;
+    }
+
+    public void setDistribution(PropertyDistributionTO distribution) {
+        this.distribution = distribution;
+    }
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(String magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
--- src/main/java/org/proygrad/picasso/rest/client/TuringClient.java
+++ src/main/java/org/proygrad/picasso/rest/client/TuringClient.java
@@ -1,6 +1,10 @@
 package org.proygrad.picasso.rest.client;
 
 import org.apache.log4j.Logger;
+import org.proygrad.picasso.rest.api.material.MaterialTO;
+import org.proygrad.picasso.rest.api.material.ParameterDistributionTO;
+import org.proygrad.picasso.rest.api.material.PropertyDistributionTO;
+import org.proygrad.picasso.rest.api.material.PropertyTO;
 import org.proygrad.picasso.rest.api.scenario.ScenarioTO;
 import org.proygrad.picasso.rest.api.user.UserTO;
 import org.proygrad.picasso.rest.exception.NotFoundException;
@@ -23,6 +27,10 @@ public class TuringClient {
 
     private final static String USER_PATH = "user/";
     private final static String USER_BY_EMAIL_PATH = "user?email=";
+
+    private final static String MATERIAL_PATH = "material";
+    private final static String MATERIAL_BY_USER_ID_PATH = "?user_id=";
+    private final static String MATERIAL_BY_PROPERTIES_PATH = "?properties=";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -71,4 +79,81 @@ public class TuringClient {
         return restTemplate.getForEntity(TURING_HOST + USER_BY_EMAIL_PATH + username, UserTO.class).getBody();
 
     }
+
+    public String addMaterial(MaterialTO material){
+        LOGGER.info("Material added " + material.getDescription() + " for " + material.getUserId());
+
+        return "material_1_mock";
+        /*TODO: A probar
+        return restTemplate.postForEntity(TURING_HOST + MATERIAL_PATH, material, String.class).getBody();
+        */
+    }
+
+    public List<MaterialTO> getMaterialsByUserIdOrProperties(String userId, List<String> properties) {
+
+        return this.getMockMaterials();
+
+        /* TODO: completar y ajustar segun usuario y propiedades q se envien
+        ResponseEntity<MaterialTO[]> responseEntity = restTemplate.getForEntity(MATERIAL_PATH + MATERIAL_BY_USER_ID_PATH + userId, MaterialTO[].class);
+        MaterialTO[] objects = responseEntity.getBody();
+
+        return new ArrayList(Arrays.asList(objects));
+*/
+    }
+
+    public MaterialTO getMaterial(String materialId) {
+        return this.giveMaterialMock(materialId);
+        /*TODO: A probar
+        return restTemplate.getForEntity(TURING_HOST + MATERIAL_PATH+ "/" + materialId, MaterialTO.class, materialId).getBody();
+        */
+    }
+
+    private List<MaterialTO> getMockMaterials(){
+
+        List<MaterialTO> mockMaterials = new ArrayList<MaterialTO>();
+
+        mockMaterials.add(giveMaterialMock("1"));
+        mockMaterials.add(giveMaterialMock("2"));
+        mockMaterials.add(giveMaterialMock("3"));
+        mockMaterials.add(giveMaterialMock("4"));
+
+        return mockMaterials;
+    }
+
+    private MaterialTO giveMaterialMock(String id){
+        List<PropertyTO> mockPropertiesTO = new ArrayList<PropertyTO>();
+
+        List<ParameterDistributionTO> parameters = new ArrayList<ParameterDistributionTO>();
+
+        ParameterDistributionTO parameterDistribution_1 = new ParameterDistributionTO();
+        parameterDistribution_1.setId("PARAMETER_DISTRIBUTION_"+id+"_ID");
+        parameterDistribution_1.setCode("PARAMETER_DISTRIBUTION_"+id+"_CODE");
+        parameterDistribution_1.setValue("VALUE");
+        parameters.add(parameterDistribution_1);
+
+        PropertyDistributionTO propertyDistribution_1 = new PropertyDistributionTO();
+        propertyDistribution_1.setId("PROPERTY_DISTRIBUTION_"+id);
+        propertyDistribution_1.setType("VARIABLE");
+        propertyDistribution_1.setParameters(parameters);
+
+        PropertyTO property_1 = new PropertyTO();
+        property_1.setId("PROPERTY_"+id);
+        property_1.setCode("CODE_PROPERTY_"+id);
+        property_1.setDistribution(propertyDistribution_1);
+        property_1.setMagnitude("MAGNITUDE");
+        property_1.setType("VARIABLE");
+        property_1.setUnit("CMS");
+        property_1.setValue(1.0d);
+
+        mockPropertiesTO.add(property_1);
+
+        MaterialTO mockMaterial_1 = new MaterialTO();
+        mockMaterial_1.setId("MATERIAL_"+id);
+        mockMaterial_1.setDescription("desc material_"+id);
+        mockMaterial_1.setProperties(mockPropertiesTO);
+        mockMaterial_1.setUserId("USER_PEPEITO_ID");
+
+        return mockMaterial_1;
+    }
+
 }
