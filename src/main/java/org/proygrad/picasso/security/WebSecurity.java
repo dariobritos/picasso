cc3091d3fbfdfa85a9e7cf043570cff51f04d896
Kolo <vidiaz@despegar.com>
2018-04-12 20:45:06 -0300

    restablecer valores originales en web security

--- src/main/java/org/proygrad/picasso/security/WebSecurity.java
+++ src/main/java/org/proygrad/picasso/security/WebSecurity.java
@@ -34,8 +34,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http/*.cors().and()*/.csrf().disable().authorizeRequests()
-                .antMatchers("/", "/rest/**","/home", "/dashboard", "/scenarios", "/scenario/**", "/materials", "/material/**", "/profile", "/new/**", "/error/**", "/login**", "/rest/login", "/assets/**", "/*.js", "/*.css", "/signup**", "/rest/sign-up","/i18n/*.json").permitAll()
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/", "/home", "/dashboard", "/scenarios", "/scenario/**", "/materials", "/material/**", "/profile", "/new/**", "/error/**", "/login**", "/rest/login", "/assets/**", "/*.js", "/*.css", "/signup**", "/rest/sign-up","/i18n/*.json").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager(), userService,mapper))
